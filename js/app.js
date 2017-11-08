dd56f91ee007bdc87c89ead57bb2396e75131c5c
Kevin-Wan <xiankai.wan@gmail.com>
2017-11-08 23:48:08 +0800

    Feature: 取消测试代码，加入timer插件源码

--- js/app.js
+++ js/app.js
@@ -2,21 +2,21 @@
  * Create a list that holds all of your cards
  */
 var cards = [
-              "fa-diamond","fa-diamond",
-              "fa-paper-plane-o","fa-paper-plane-o",
-              "fa-anchor","fa-anchor",
-              "fa-bolt","fa-bolt",
-              "fa-cube","fa-cube",
-              "fa-leaf","fa-leaf",
-              "fa-bicycle","fa-bicycle",
-              "fa-bomb","fa-bomb"
-            ];
+  "fa-diamond", "fa-diamond",
+  "fa-paper-plane-o", "fa-paper-plane-o",
+  "fa-anchor", "fa-anchor",
+  "fa-bolt", "fa-bolt",
+  "fa-cube", "fa-cube",
+  "fa-leaf", "fa-leaf",
+  "fa-bicycle", "fa-bicycle",
+  "fa-bomb", "fa-bomb"
+];
 var steps = 0;
 var nosame = [];
 var onestar = 14;
 var twostar = 22;
 var threestar = 33;
-$(function(){
+$(function () {
   initGamePage();
   $('li.card').click(function () {
     nosame.push($("ul li").index(this));
@@ -36,55 +36,55 @@ $(function(){
           checkMatch(cards);
         }
         // remove the start
-       switch(steps){
-         case onestar:
-           $('ul.stars').find('i').eq(0).addClass('animated bounceOutUp');
-           break;
-         case twostar:
-           $('ul.stars').find('i').eq(1).addClass('animated bounceOutUp');
-           break;
-         case threestar:
-           $('ul.stars').find('i').eq(2).addClass('animated bounceOutUp');
-           break;
-       }
-    }
+        switch (steps) {
+          case onestar:
+            $('ul.stars').find('i').eq(0).addClass('animated bounceOutUp');
+            break;
+          case twostar:
+            $('ul.stars').find('i').eq(1).addClass('animated bounceOutUp');
+            break;
+          case threestar:
+            $('ul.stars').find('i').eq(2).addClass('animated bounceOutUp');
+            break;
+        }
+      }
     }
   });
 });
 
 // 定义匹配函数
-function checkMatch(checkArry){
+function checkMatch(checkArry) {
   var class1 = checkArry[0];
   var class2 = checkArry[1];
   var SelectClass1 = '.' + class1.slice(3); //用于之后方便搜索
   var SelectClass2 = '.' + class2.slice(3);
-  if (class1 == class2){
-    $(SelectClass1).each(function(){
+  if (class1 == class2) {
+    $(SelectClass1).each(function () {
       $(this).parent().toggleClass('match animated rubberBand');
       $(this).parent().unbind();
     });
-  }else{
+  } else {
     $('.open').find(SelectClass1).parent().addClass('animated shake nomatch');
     $('.open').find(SelectClass2).parent().addClass('animated shake nomatch');
-    setTimeout(function(){
+    setTimeout(function () {
       $('.open').find(SelectClass1).parent().removeClass('animated nomatch shake open show');
       $('.open').find(SelectClass2).parent().removeClass('animated nomatch shake open show');
-    },1000);
-    checkArry.splice(0,2);
+    }, 1000);
+    checkArry.splice(0, 2);
   }
   if (checkArry.length == 16) {
     isWin();
   }
 }
 // more easy way for shuffle
-function shuffle(array){
-  array.sort(function(){
-    return Math.random() > 0.5? 1:-1;
+function shuffle(array) {
+  array.sort(function () {
+    return Math.random() > 0.5 ? 1 : -1;
   });
   return array;
 }
 //ready the initial page info 
-function initGamePage(){
+function initGamePage() {
   var initArry = shuffle(cards);
   $('.card i').each(function () {
     // $(this).parent().removeClass(); //这里这么做是帮助restart节省代码，不然要额外清除open show等class
@@ -110,7 +110,7 @@ $('ul.deck').one('click', function () {
 });
 
 // isWin function
-function isWin(){
+function isWin() {
   stopclock();
   setTimeout(function () {
     $('#win').toggleClass('hidden animated bounceInDown');
@@ -142,18 +142,32 @@ $('.restart').click(function () {
 // play again function
 $('#winAgain').click(function () {
   $('#win').toggleClass('bounceInDown bounceOutUp');
-  setTimeout(function(){
+  setTimeout(function () {
     $('#win').toggleClass('hidden');
     window.location.reload();
-  },700);
+  }, 700);
 });
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+
+// jquery timer 插件代码
+jQuery.fn.extend({ everyTime: function (c, a, d, b) { return this.each(function () { jQuery.timer.add(this, c, a, d, b) }) }, oneTime: function (c, a, d) { return this.each(function () { jQuery.timer.add(this, c, a, d, 1) }) }, stopTime: function (c, a) { return this.each(function () { jQuery.timer.remove(this, c, a) }) } });
+jQuery.extend({
+  timer: {
+    global: [], guid: 1, dataKey: "jQuery.timer", regex: /^([0-9]+(?:\.[0-9]*)?)\s*(.*s)?$/, powers: { ms: 1, cs: 10, ds: 100, s: 1E3, das: 1E4, hs: 1E5, ks: 1E6 }, timeParse: function (c) { if (c == undefined || c == null) return null; var a = this.regex.exec(jQuery.trim(c.toString())); return a[2] ? parseFloat(a[1]) * (this.powers[a[2]] || 1) : c }, add: function (c, a, d, b, e) {
+      var g = 0; if (jQuery.isFunction(d)) { e || (e = b); b = d; d = a } a = jQuery.timer.timeParse(a); if (!(typeof a != "number" || isNaN(a) || a < 0)) {
+        if (typeof e != "number" || isNaN(e) || e < 0) e =
+          0; e = e || 0; var f = jQuery.data(c, this.dataKey) || jQuery.data(c, this.dataKey, {}); f[d] || (f[d] = {}); b.timerID = b.timerID || this.guid++; var h = function () { if (++g > e && e !== 0 || b.call(c, g) === false) jQuery.timer.remove(c, d, b) }; h.timerID = b.timerID; f[d][b.timerID] || (f[d][b.timerID] = window.setInterval(h, a)); this.global.push(c)
+      }
+    }, remove: function (c, a, d) {
+      var b = jQuery.data(c, this.dataKey), e; if (b) {
+        if (a) {
+          if (b[a]) {
+            if (d) { if (d.timerID) { window.clearInterval(b[a][d.timerID]); delete b[a][d.timerID] } } else for (d in b[a]) {
+              window.clearInterval(b[a][d]);
+              delete b[a][d]
+            } for (e in b[a]) break; if (!e) { e = null; delete b[a] }
+          }
+        } else for (a in b) this.remove(c, a, d); for (e in b) break; e || jQuery.removeData(c, this.dataKey)
+      }
+    }
+  }
+}); jQuery(window).bind("unload", function () { jQuery.each(jQuery.timer.global, function (c, a) { jQuery.timer.remove(a) }) });
